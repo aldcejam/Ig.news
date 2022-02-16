96a921640f1f2a9cc35deb3d3bc473b307fee684
Aldcejam <aldcejamj923@gmail.com>
2022-02-16 18:40:50 -0300

    init api

--- src/components/SingInButton/SingInButton.tsx
+++ src/components/SingInButton/SingInButton.tsx
@@ -1,25 +1,27 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from "react-icons/fi"
 import styles from "./SingInbutton.module.scss"
-
+import { signIn, useSession } from 'next-auth/react'
 
 export function SingInButton() {
-    const isUserLoggedIn = true;
+    const { data: session, status } = useSession();
 
-    return isUserLoggedIn ? ((
+
+    return session ? ((
         <button
             type="button"
             className={styles.singInButton}
         >
             <FaGithub color="#04d361" />
             PessoasName
-            <FiX color="#737380" className={styles.closeIcon}/>
+            <FiX color="#737380" className={styles.closeIcon} />
         </button>
     )) : (
         (
             <button
                 type="button"
                 className={styles.singInButton}
+                onClick={()=> signIn('github')}
             >
                 <FaGithub color="#eba417" />
                 Sing in with Github
